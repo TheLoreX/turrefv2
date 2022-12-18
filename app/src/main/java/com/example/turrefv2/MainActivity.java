72e62491024f84e5f01870ad871060da75ba1149
Silence <tokgozselim2016@gmail.com>
2022-12-18 19:50:20 +0100

    on working

--- app/src/main/java/com/example/turrefv2/MainActivity.java
+++ app/src/main/java/com/example/turrefv2/MainActivity.java
@@ -16,7 +16,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.turrefv2.databinding.ActivityMainBinding;
-
+sa
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
